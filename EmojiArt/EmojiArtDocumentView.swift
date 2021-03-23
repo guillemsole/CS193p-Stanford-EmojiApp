3830c83263108b51f9042dee309abf575e38078f
guillemsole <guillemsolecubilo@gmail.com>
2021-03-23 11:04:45 +0100

    ADD ImagePicker to choose device images

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -62,7 +62,7 @@ struct EmojiArtDocumentView: View {
                     location = CGPoint(x: location.x / zoomScale, y: location.y / zoomScale)
                     return drop(providers: providers, at: location)
                 }
-                .navigationBarItems(trailing: Button(action: {
+                .navigationBarItems(leading: pickImage, trailing: Button(action: {
                     if let url = UIPasteboard.general.url, url != document.backgroundURL {
                         confirmBackgroundPaste = true
                     } else {
@@ -87,6 +87,37 @@ struct EmojiArtDocumentView: View {
             }, secondaryButton: .cancel())
         }
     }
+    
+    @State private var showImagePicker: Bool = false
+    @State private var imagePickerSourceType = UIImagePickerController.SourceType.photoLibrary
+    
+    private var pickImage: some View {
+        HStack {
+            Image(systemName: "photo").imageScale(.large).foregroundColor(.accentColor).onTapGesture {
+                imagePickerSourceType = .photoLibrary
+                showImagePicker = true
+                
+            }
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                Image(systemName: "camera").imageScale(.large).foregroundColor(.accentColor).onTapGesture {
+                    imagePickerSourceType = .camera
+                    showImagePicker = true
+                }
+            }
+        }
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(sourceType: imagePickerSourceType) { image in
+                if image != nil {
+                    DispatchQueue.main.async {
+                        self.document.backgroundURL = image!.storeInFilesystem()
+                    }
+                }
+                showImagePicker = false
+            }
+        }
+
+    }
+    
     
     @State private var explainBackgroundPaste = false
     @State private var confirmBackgroundPaste = false
